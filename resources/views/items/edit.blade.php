2f48aa64d8e7a8ce957eb8cb1bd7874f2ccedb87
Jedy24 <reihimawari27@gmail.com>
2023-11-24 12:37:44 +0700

    CRUD update

--- resources/views/items/edit.blade.php
+++ resources/views/items/edit.blade.php
@@ -3,5 +3,60 @@
 @section('title', 'Edit Data Item')
 
 @section('content')
+    <div class="mt-4 p-5 bg-black text-white rounded">
+        <h1>Edit Data Item</h1>
+    </div>
 
+    @if ($errors->any())
+        <div class="alert alert-danger mt-4">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    <div class="row my-5">
+        <div class="col-12 px-5">
+            <form action="{{ route('items.update', ['item' => $item->id]) }}" method="POST" enctype="multipart/form-data">
+                @method('PUT')
+                @csrf
+
+                <div class="mb-3 col-md-12 col-sm-12">
+                    <label for="title" class="form-label">ID</label>
+                    <input type="text" class="form-control @error('id') invalid @enderror" name="id" id="id" value="{{ old('id') ?? $item->id }}" readonly>
+                    @error('id')
+                        <div class="text-danger">{{ $message }}</div>
+                    @enderror
+                </div>
+
+                <div class="mb-3 col-md-12 col-sm-12">
+                    <label for="title" class="form-label">Nama</label>
+                    <input type="text" class="form-control  @error('nama') invalid @enderror" name="nama" id="nama" value="{{ old('nama') ?? $item->nama }}">
+                    @error('nama')
+                        <div class="text-danger">{{ $message }}</div>
+                    @enderror
+                </div>
+
+                <div class="mb-3 col-md-12 col-sm-12">
+                    <label for="title" class="form-label">Harga</label>
+                    <input type="number" class="form-control  @error('harga') invalid @enderror" name="harga" id="harga" min="0" value="{{ old('harga') ?? $item->harga }}">
+                    @error('harga')
+                        <div class="text-danger">{{ $message }}</div>
+                    @enderror
+                </div>
+
+                <div class="mb-3 col-md-12 col-sm-12">
+                    <label for="title" class="form-label">Stok</label>
+                    <input type="number" class="form-control  @error('stok') invalid @enderror" name="stok" id="stok" min="0" value="{{ old('stok') ?? $item->stok }}">
+                    @error('stok')
+                        <div class="text-danger">{{ $message }}</div>
+                    @enderror
+                </div>
+
+                <button type="submit" class="btn btn-primary btn-block mt-3">Save</button>
+            </form>
+        </div>
+    </div>
 @endsection
