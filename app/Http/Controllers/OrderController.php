49e06341cd223ad485288eb90e8182ac7ce0e5f1
Jedy24 <reihimawari27@gmail.com>
2023-11-25 23:17:16 +0700

    Order Controller

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Order;
 use App\Models\Item;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class OrderController extends Controller
 {
@@ -14,61 +15,67 @@ class OrderController extends Controller
         return view('order', compact('items'));
     }
 
-    public function createOrder(Request $request){
-        /*Validasi data*/
-        $validated = $request->validate([
-            'status' => 'required|in:Selesai,Menunggu Pembayaran',
-            'items.*.item_id' => 'required|exists:items,id',
-            'items.*.quantity' => 'required|integer|min:1',
-        ]);
+    public function createOrder(Request $request)
+    {
+        $inputs = $request->all();
 
-        /*Menghitung harga dengan asumsi pajak 11%*/
-        $totalHarga = 0;
-        foreach ($validated['items'] as $itemData) {
-            $item = Item::find($itemData['item_id']);
-            $totalHarga += $item->harga * $itemData['quantity'];
-        }
-        $totalHargaPajak = $totalHarga * 1.11;
+        /*Mengambil ID & Quantity dari input request*/
+        $ids = $request->get('.nama');
+        $quantities = $request->get('.quantity');
 
-        /*Membuat Order*/
-        $order = Order::create([
-            'status' => $validated['status'],
-            'total_harga' => $totalHargaPajak,
-        ]);
+        /*Validasi jumlah stok*/
+        // foreach ($ids as $key => $id) {
+        //     $item = Item::find($id);
 
-        /*Membuat Order Item*/
-        foreach ($validated['items'] as $itemData) {
-            $order->items()->create([
-                'item_id' => $itemData['item_id'],
-                'quantity' => $itemData['quantity'],
-            ]);
+        //     /*Cek stok item dengan permintaan*/
+        //     if (!$item || $item->stok < $quantities[$key]) {
+        //         return redirect()->route('order')->with('error', 'Insufficient Stok');
+        //     }
+        // }
 
-            /*Update Stok Item*/
-            $item = Item::find($itemData['item_id']);
-            $item->stok -= $itemData['quantity'];
-            $item->save();
-        }
+        /*Membuat Order ke DB*/
+        // DB::beginTransaction();
+
+        $order = new Order;
+        $order->status = $request->get('status');
+        $order->save();
+
+        /*Error duplicate primary key karena input 2 item yang sama berdasarkan ID.
+          Data tidak tersimpan di table order_item.*/
+
+        /*Menyimpan detil order di table order_item*/
+        // foreach ($ids as $key => $id) {
+        //     $quantity = isset($quantities[$key]) ? $quantities[$key] : null;
+
+        //     DB::table('order_item')->insert([
+        //         'order_id' => $order->id,
+        //         'item_id' => $id,
+        //         'quantity' => $quantity,
+        //     ]);
+        // }
+
+        // DB::commit();
 
         return redirect()->route('index')->with('success', 'Order has been created successfully');
     }
 
-//     public function list(){
-//         /*Menampilkan seluruh daftar order*/
-//         $orders = Order::all();
-//         return view('list', compact('orders'));
-//     }
+    public function list(){
+        /*Menampilkan seluruh daftar order*/
+        $orders = Order::all();
+        return view('list', compact('orders'));
+    }
 
-//     public function detail(Order $order)
-//     {
-//         $details = Order::join('order_item', 'orders.id', '=', 'order_item.order_id')
-//             ->join('items', 'order_item.item_id', '=', 'items.id')
-//             ->get(['order_item.item_id', 'order_item.quantity', 'items.nama', 'items.harga']);
+    public function detail(Order $order)
+    {
+        $details = Order::join('order_item', 'orders.id', '=', 'order_item.order_id')
+            ->join('items', 'order_item.item_id', '=', 'items.id')
+            ->get(['order_item.item_id', 'order_item.quantity', 'items.nama', 'items.harga']);
 
-//         $total = 0;
-//         foreach ($details as $detail) {
-//             $total += $detail->quantity * $detail->harga;
-//         }
+        $total = 0;
+        foreach ($details as $detail) {
+            $total += $detail->quantity * $detail->harga;
+        }
 
-//         return view('details', compact('order', 'details', 'total'));
-//     }
+        return view('details', compact('order', 'details', 'total'));
+    }
 }
