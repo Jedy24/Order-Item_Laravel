10d3a035357d6aeed799d150bd98db89cb75cd9f
Jedy24 <reihimawari27@gmail.com>
2023-11-28 21:01:40 +0700

    Index page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,61 +3,27 @@
 @section('title', 'Welcome Page')
 
 @section('content')
-    <div class="p-4 p-md-5 mb-4 text-white rounded bg-dark">
-        <div class="col-md-6 px-0">
-            <h1 class="display-4 fst-italic">Title of a longer featured blog post</h1>
-            <p class="lead my-3">Multiple lines of text that form the lede, informing new readers quickly and
-                efficiently about what’s most interesting in this post’s contents.</p>
-            <p class="lead mb-0"><a href="#" class="text-white fw-bold">Continue reading...</a></p>
-        </div>
+    <div class="mt-4 p-5 bg-black text-white rounded">
+        <h1>Order Details</h1>
     </div>
 
-    <div class="row mb-2">
-        <div class="col-md-6">
-            <div
-                class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-                <div class="col p-4 d-flex flex-column position-static">
-                    <strong class="d-inline-block mb-2 text-primary">World</strong>
-                    <h3 class="mb-0">Featured post</h3>
-                    <div class="mb-1 text-muted">Nov 12</div>
-                    <p class="card-text mb-auto">This is a wider card with supporting text below as a natural
-                        lead-in to additional content.</p>
-                    <a href="#" class="stretched-link">Continue reading</a>
-                </div>
-                <div class="col-auto d-none d-lg-block">
-                    <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg"
-                        role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice"
-                        focusable="false">
-                        <title>Placeholder</title>
-                        <rect width="100%" height="100%" fill="#55595c"></rect><text x="50%"
-                            y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
-                    </svg>
-
-                </div>
+    <div class="d-flex justify-content-center mt-3">
+        <div class="card border-0 mx-3" style="width: 33rem; box-shadow: 0 8px 16px rgba(82, 149, 236, 0.7);">
+            <div class="card-body">
+                <p class="card-title fw-bold display-6">Item count</p>
+                <h3 class="card-text">{{ $countItem }}</h3>
+                <a href="{{ route('items.index') }}" class="btn btn-primary mt-3">View All Items</a>
             </div>
         </div>
-        <div class="col-md-6">
-            <div
-                class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-                <div class="col p-4 d-flex flex-column position-static">
-                    <strong class="d-inline-block mb-2 text-success">Design</strong>
-                    <h3 class="mb-0">Post title</h3>
-                    <div class="mb-1 text-muted">Nov 11</div>
-                    <p class="mb-auto">This is a wider card with supporting text below as a natural lead-in to
-                        additional content.</p>
-                    <a href="#" class="stretched-link">Continue reading</a>
-                </div>
-                <div class="col-auto d-none d-lg-block">
-                    <svg class="bd-placeholder-img" width="200" height="250"
-                        xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail"
-                        preserveAspectRatio="xMidYMid slice" focusable="false">
-                        <title>Placeholder</title>
-                        <rect width="100%" height="100%" fill="#55595c"></rect><text x="50%"
-                            y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
-                    </svg>
-                </div>
+
+        <div class="card border-0 mx-3" style="width: 33rem; box-shadow: 0 8px 16px rgba(82, 149, 236, 0.7);">
+            <div class="card-body">
+                <p class="card-title fw-bold display-6">Order count</p>
+                <h3 class="card-text">{{ $countOrder }}</h3>
+                <a href="{{ route('orders.index') }}" class="btn btn-primary mt-3">View All Orders</a>
             </div>
         </div>
     </div>
+
 @endsection
 
