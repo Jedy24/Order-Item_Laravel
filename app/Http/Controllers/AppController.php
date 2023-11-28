0e6da6bcacffd6cbc67a149977ce95912c0510c9
Jedy24 <reihimawari27@gmail.com>
2023-11-28 21:01:33 +0700

    Controller index

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -2,11 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Item;
+use App\Models\Order;
+
 use Illuminate\Http\Request;
 
 class AppController extends Controller
 {
     public function index(){
-        return view('index');
+
+        $countItem = Item::all()->count();
+        $countOrder = Order::all()->count();
+
+        return view('index', compact('countItem', 'countOrder'));
     }
 }
