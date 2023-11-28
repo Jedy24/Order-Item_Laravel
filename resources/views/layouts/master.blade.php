0627adc348b85f6a982c2b66a9d39add4cafa835
Jedy24 <reihimawari27@gmail.com>
2023-11-28 21:25:17 +0700

    Template

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -12,27 +12,18 @@
 <body>
     <div class="container">
         <header class="blog-header py-3">
-            <div class="col-12 text-center">
-                <a class="blog-header-logo text-dark" href="{{ route('index') }}">Order Items</a>
+            <div class="row flex-nowrap justify-content-between align-items-center">
+                <div class="col-2">
+                    <a class="btn btn-primary shadow" href="{{ route('items.index') }}">Items</a>
+                </div>
+                <div class="col-8 text-center">
+                    <a class="blog-header-logo text-dark" href="{{ route('index') }}">Order Items</a>
+                </div>
+                <div class="col-2 text-end">
+                    <a class="btn btn-primary shadow" href="{{ route('orders.create') }}">Orders</a>
+                </div>
             </div>
         </header>
-
-        <div class="nav-scroller py-1 mb-2">
-            <nav class="nav d-flex justify-content-between">
-                <a class="p-2 link-secondary" href="#">World</a>
-                <a class="p-2 link-secondary" href="#">U.S.</a>
-                <a class="p-2 link-secondary" href="#">Technology</a>
-                <a class="p-2 link-secondary" href="#">Design</a>
-                <a class="p-2 link-secondary" href="#">Culture</a>
-                <a class="p-2 link-secondary" href="#">Business</a>
-                <a class="p-2 link-secondary" href="#">Politics</a>
-                <a class="p-2 link-secondary" href="#">Opinion</a>
-                <a class="p-2 link-secondary" href="#">Science</a>
-                <a class="p-2 link-secondary" href="#">Health</a>
-                <a class="p-2 link-secondary" href="#">Style</a>
-                <a class="p-2 link-secondary" href="#">Travel</a>
-            </nav>
-        </div>
     </div>
 
     <main class="container">
